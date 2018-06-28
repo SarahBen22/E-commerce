907d53c6082706e8f41e6df183c13ecec91b06ef
O'Hara <julien.huvier@gmail.com>
2018-06-28 12:54:57 +0200

    home

--- views/home.php
+++ views/home.php
@@ -1,4 +1,59 @@
 
 <?php
-require_once "views/navbar.php";
+require_once "./navbar.php";
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="../deco.css">
+<link href="https://fonts.googleapis.com/css?family=Advent+Pro" rel="stylesheet">
+
+<script type="text/javascript" src="../Jquery/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="../main.js"></script>	
+</head>
+<body>
+
+
+
+
+
+<div class="carousel">
+  <img src="http://placehold.it/600x350/faf345/000000&text=item 1" alt="Image 1" />
+  <img src="http://placehold.it/600x350/ffa345/000000&text=item 2" alt="Image 2" />
+  <img src="http://placehold.it/600x350/ffa3ee/000000&text=item 3" alt="Image 3" />
+  <img src="http://placehold.it/600x350/ffd3ee/000000&text=item 4" alt="Image 4" />
+  <img src="http://placehold.it/600x350/fda30e/000000&text=item 5" alt="Image 5" />
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script type="text/javascript" src="../main.js"></script>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
