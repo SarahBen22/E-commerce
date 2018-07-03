c03ad92bf5c9d17221ea4f8284cd4175ebf823ba
O'Hara <julien.huvier@gmail.com>
2018-07-03 16:16:39 +0200

    cordialement

--- views/navbar.php
+++ views/navbar.php
@@ -1,14 +1,14 @@
 
 
 	<div id="bandeau">
-		<img id="logo" src="/Site_e-commerce/img/arcade-logo.png">
+		<img id="logo" src="../img/arcade-logo.png">
 		
-	echo	<a class="moncompte" href=\"/Site_e-commerce/index.php/profil_client\">Mon Compte</a>
+		<a class="moncompte" href=\"/Site_e-commerce/index.php/profil_client\">Mon Compte</a>
 	</div>
 	<div id="navbar">
 
 		<ul id="menu">
-		  	<li><a class="home" href="default.html">Home</a></li>
+		  	<li><a class="home" href="../img/default.html">Home</a></li>
 
 		  	<li><a href="news.asp">Par Plateforme</a>
 		  		<ul class="plateforme">
@@ -38,11 +38,11 @@
 		  	<li><a href="about.asp">Mieux Notés</a></li>
 		  	<li><a href="about.asp">Offres Spéciales</a></li>
 		  	<li><a href="about.asp">Goodies</a></li>
-<<<<<<< HEAD
+
 		  	<li  class="panier"> <a  onclick="toggleForm()" href=""><img class="basket" src="../img/cart.png" width="35px" height="35px" ></a></li>
-=======
+
 		  	<li class="panier"><a  href="about.asp"><img class="basket" src="/Site_e-commerce/img/cart.png" width="35px" height="35px" ></a></li>
->>>>>>> 36a84c4e805bdde6d1eab60bea133ef7a8f769a2
+
 		 	<li id="searchbar">
 		  	<form>
 				<input type="search" placeholder="recherche" aria-label="Search" >
@@ -60,11 +60,8 @@
 
 
 
-<<<<<<< HEAD
-=======
 
 
 <?php
->>>>>>> 36a84c4e805bdde6d1eab60bea133ef7a8f769a2
 
 ?>
