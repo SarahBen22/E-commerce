ef56c4faab82377f67e266dd3653f475be87c2e1
Sarah <sa.tahiris@gmail.com>
2018-07-09 09:19:43 +0100

    ok

--- controllers/profil_client.php
+++ controllers/profil_client.php
@@ -4,7 +4,7 @@ require_once "models/profil_client.php";
 // on teste si le visiteur a soumis le formulaire
 if (true) {// isset= permet de voir si une variable est définie
   // on teste l'existence de nos variables. On teste également si elles ne sont pas vides
-  if (isset($_POST['mail'])) {
+  if (isset($_POST['mail'])) {// on cherche à savoir si la pers a appuyé sur le bouton
 
      $date_de_naissance = (isset($_POST['date_de_naissance'])) ?  $_POST['date_de_naissance'] : '1907-07-07';
 
